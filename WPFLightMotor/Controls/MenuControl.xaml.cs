169f0ec04b22978f2c4507a6b189bd396a73208a
kaiser <kaiser.lacko@gmail.com>
2023-11-17 09:25:27 +0100

    Added controls

---
+++ WPFLightMotor/Controls/MenuControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WPFLightMotor.Controls;
+
+public partial class MenuControl : UserControl
+{
+    public MenuControl()
+    {
+        InitializeComponent();
+    }
+}
