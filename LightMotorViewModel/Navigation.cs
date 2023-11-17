17939a4087fbaab2df0fe92422c0d4c8235ce685
kaiser <kaiser.lacko@gmail.com>
2023-11-17 09:25:43 +0100

    Navigation singleton

---
+++ LightMotorViewModel/Navigation.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using LightMotorViewModel;
+
+namespace WPFLightMotor;
+
+public class Navigation
+{
+    private static Navigation? instance;
+    
+    private Navigation(){}
+
+    public static Navigation Get()
+    {
+        instance ??= new Navigation();
+
+        return instance;
+    }
+    
+    private ViewModelBase _currentViewModel;
+
+    public ViewModelBase CurrentViewModel
+    {
+        get => _currentViewModel;
+        set
+        {
+            _currentViewModel = value;
+            OnCurrentViewChanged();
+        }
+    }
+
+    public event Action CurrentViewChanged;
+
+    protected virtual void OnCurrentViewChanged()
+    {
+        CurrentViewChanged?.Invoke();
+    }
+}
