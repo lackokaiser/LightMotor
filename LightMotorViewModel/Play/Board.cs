0b6f79b7b76fedad95ac0803013daeffae7dec0e
kaiser <kaiser.lacko@gmail.com>
2023-11-19 11:24:06 +0100

    Game board added
    Represents the game's state

---
+++ LightMotorViewModel/Play/Board.cs
@@ -0,0 +1,27 @@
+using System.Collections.ObjectModel;
+using LightMotorViewModel.ViewModel;
+
+namespace LightMotorViewModel.Play;
+
+public class Board : ViewModelBase
+{
+    private int _size;
+    private ObservableCollection<EntityViewModel> _entities = new ();
+
+    public int Size
+    {
+        get => _size;
+        set => Set(ref _size, value);
+    }
+
+    public ObservableCollection<EntityViewModel> Entities
+    {
+        get => _entities;
+        set => Set(ref _entities, value);
+    }
+
+    public Board(int size)
+    {
+        _size = size;
+    }
+}
