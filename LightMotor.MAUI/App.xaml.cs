491186a159329c2d28310030ec974a6527d604af
kaiser <kaiser.lacko@gmail.com>
2023-12-01 11:32:43 +0100

    Main app start classes

---
+++ LightMotor.MAUI/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using LightMotor.Persistence;
+using LightMotor.Root;
+using LightMotor.ViewModel.ViewModel;
+
+namespace LightMotor.MAUI;
+
+public partial class App
+{
+    private readonly Game _model = new(new MauiPersistenceProvider());
+    
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell(ref _model)
+        {
+            BindingContext = new MenuViewModel(ref _model)
+        };
+
+    }
+}
---
+++ LightMotor.MAUI/AppShell.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LightMotor.Event;
+using LightMotor.MAUI.View;
+using LightMotor.Root;
+using LightMotor.ViewModel;
+using LightMotor.ViewModel.EventArgs;
+using GameViewModel = LightMotor.ViewModel.ViewModel.GameViewModel;
+
+namespace LightMotor.MAUI;
+
+public partial class AppShell
+{
+    private Game _game;
+
+    public AppShell(ref Game game)
+    {
+        _game = game;
+        InitializeComponent();
+        
+        ViewCallback.Get().OpenFileMessenger += OnOpenFileMessenger;
+        ViewCallback.Get().ShowMessageMessenger += OnShowMessageMessenger;
+        
+        game.OnGameInitialized += OnGameInitialized;
+    }
+
+    private async void OnGameInitialized(object obj, GameInitializedEventArgs e)
+    {
+        var gameVm = new GameViewModel(ref _game, e);
+        GamePage page = new GamePage()
+        {
+            BindingContext = gameVm
+        };
+
+        await Navigation.PushAsync(page);
+        
+        if (e.GameStatus is PlayStatus)
+            await gameVm.Start();
+    }
+
+    private async void OnShowMessageMessenger(object obj, ShowMessageEventArgs args)
+    {
+        await DisplayAlert(args.Caption, args.Text, "Ok");
+    }
+
+    private void OnOpenFileMessenger()
+    {
+        
+    }
+}
