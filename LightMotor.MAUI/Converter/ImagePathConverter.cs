4e9a33a9c06aa9ff2e8526ddb3fd72883483d338
kaiser <kaiser.lacko@gmail.com>
2023-12-08 12:49:40 +0100

    Converter for image path converter
    properly changing the string to access the image files

---
+++ LightMotor.MAUI/Converter/ImagePathConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace LightMotor.MAUI.Converter;
+
+public class ImagePathConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return String.Empty;
+        string? str = value.ToString();
+        if (string.IsNullOrEmpty(str))
+            return String.Empty;
+        return str.Substring(str.LastIndexOf('/')+1);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return "Resources/Images/" + value;
+    }
+}
