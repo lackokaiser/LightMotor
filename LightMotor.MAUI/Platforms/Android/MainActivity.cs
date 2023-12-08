9def833b4ca86f33c059d8547a13e1a8f3f127e6
kaiser <kaiser.lacko@gmail.com>
2023-12-08 12:48:47 +0100

    Platform changes
    Only portrait mode allowed in application

--- LightMotor.MAUI/Platforms/Android/MainActivity.cs
+++ LightMotor.MAUI/Platforms/Android/MainActivity.cs
@@ -4,7 +4,8 @@ using Android.OS;
 
 namespace LightMotor.MAUI
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, 
+        ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
